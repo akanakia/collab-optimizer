c78defa49dd6b119b3019eed9d5e1b0e4a1aad97
Anshul Kanakia <kanakia@colorado.edu>
2015-07-07 19:25:35 -0600

    moved teh fire making over to the fire maker class.

--- droplet-ta-exp/FireMaker.py
+++ droplet-ta-exp/FireMaker.py
@@ -1,4 +1,102 @@
 # -*- coding: utf-8 -*-
+import random
 
 class FireMaker:
+
+    class Cell:   
+        # Cell status Enum
+        FRONT = 0
+        CORE  = 1
+        BURNT = 2          
+        
+        def __init__(self, row, col):
+            """
+            Cell constructor
+            """
+            self.col = col
+            self.row = row
+            self.i = 1
+            self.status = FireMaker.Cell.FRONT
+            self._allowed_neighbors = None
     
+        def get_allowed_neighbors(self, num_cell_rows, num_cell_cols):
+            """
+            Returns a list of tuples with coordinates of the allowed neighbors of 
+            this cell
+            """
+            if self._allowed_neighbors is None:
+                self._allowed_neighbors = []
+                
+                for colp in [self.col - 1, self.col, self.col + 1]:
+                    if self.col - 1 < 0 or self.col + 1 == num_cell_cols:
+                        continue
+                    for rowp in [self.row - 1, self.row, self.row + 1]:
+                        if self.row - 1 < 0 or self.row + 1 == num_cell_rows:
+                            continue
+                        if self.col == colp and self.row == rowp:
+                            continue
+                        self._allowed_neighbors.append((colp, rowp))
+                        
+            return self._allowed_neighbors
+    
+    
+    def __init__(self, cell_height, cell_width, num_cell_rows, num_cell_cols):
+        """
+        FireMaker Constructor
+        """
+        self.num_cell_rows = num_cell_rows
+        self.num_cell_cols = num_cell_cols
+        self.cell_width = cell_width
+        self.cell_height = cell_height
+        self._active_cells = []
+
+    def add_cell(self, row, col):
+        """
+        Add a new fire cell placed at location (row, col) on the grid. It
+        returns True if a new cell was created, False if a fire cell already 
+        existed at that location.
+        """
+        pass
+
+    def add_cell_random(self):
+        """
+        Adds a new fire cell randomlly placed on the grid. It will only add a
+        new cell if less than 75% of the total area is currently covered by 
+        fire. Returns True on success
+        """
+        if len(self._active_cells) > (self.num_cell_cols * self.num_cell_rows * .75):
+            return False
+            
+        if len(self._active_cells) > 0:
+            occupied_cells = [(cell.row, cell.col) for cell in self._active_cells]
+            (new_row, new_col) = occupied_cells[0]
+            while((new_row, new_col) in occupied_cells):
+                (new_row, new_col) = (random.randint(0, self.num_cell_rows - 1), random.randint(0, self.num_cell_cols - 1))
+            self._active_cells.append(FireMaker.Cell(new_row, new_col))
+        else:
+            self._active_cells.append(FireMaker.Cell(random.randint(0, self.num_cell_rows - 1), random.randint(0, self.num_cell_cols - 1)))
+            
+        return True
+    
+    def propogate_fire(self):
+        """
+        Propogates the fire from existing fire cells to neighboring cells using
+        the desired probability distribution.
+        """
+        front_cells = [cell for cell in self._active_cells if cell.status == Cell.FRONT]    
+    
+    def increment_intensity(self, inc=1):
+        """
+        Increments the intensity of all the fire cells by the amount specified 
+        by inc (=1 by default) up to a max of 255.
+        inc souhld be a postive integer.
+        """
+        for cell in self._active_cells:
+            cell.i = min(255, cell.i + inc)
+
+    def get_fire_grid(self):
+        """
+        Returns a list of the 3-tuples (row, col, intensity) comprising the 
+        grid where cells with fire are present
+        """
+        return [(cell.row, cell.col, cell.i) for cell in self._active_cells]
--- droplet-ta-exp/droplet-ta-exp.py
+++ droplet-ta-exp/droplet-ta-exp.py
@@ -1,57 +1,26 @@
 # -*- coding: utf-8 -*-
 import pygame
-import random
 from pygame.locals import *
-import pygame.gfxdraw
+from FireMaker import *
 
-FIRE_EXPAND_INTERVAL = .2
 FPS = 60
-
-class Cell:
-
-    # Cell status Enum
-    FRONT = 0
-    CORE  = 1
-    BURNT = 2          
-    
-    def __init__(self, (x_coord, y_coord), i, status):
-        self.x = x_coord
-        self.y = y_coord
-        self.i = i
-        self.status = status
-        self._max_neighbors = None
-        
-    def get_max_cell_neighbors(self, (num_cells_x, num_cells_y)):
-        if self._max_neighbors is None:
-            if (self.x == 0 or self.x == (num_cells_x - 1)) and (self.y == 0 or self.y == (num_cells_y - 1)):
-                self._max_neighbors = 3 # corner cell
-            elif self.x == 0 or self.x == num_cells_x - 1 or self.y == 0 or self.y == num_cells_y - 1:
-                self._max_neighbors = 5 # border cell
-            else:
-                self._max_neighbors = 8 # internal cell
-        
-        return self._max_neighbors
-
+SCREEN_X = 800
+SCREEN_Y = 600    
+CELL_W = 10
+CELL_H = 10
 
 def main():
+
+    # Set up the FireMaker class and add a randomly located fire seed
+    fm = FireMaker(CELL_H, CELL_W, SCREEN_Y / CELL_H, SCREEN_X / CELL_W)
+    fm.add_cell_random()
     
-    screen_x = 800
-    screen_y = 600
-    cell_width = 10
-    cell_height = 10
-    num_cells_x = screen_x / cell_width
-    num_cells_y = screen_y / cell_height
-    
-    # Stores the tuple: ((x, y) , i) where:
-    # (x, y) is the coordinate of the cell (0, 0) is top left
-    # i is the intensity of a fire cell from 0 to 255. 
-    active_cells = [Cell((random.randint(0, num_cells_x - 1),random.randint(0, num_cells_y - 1)), 0, Cell.FRONT)]
-    
+    # Set up PyGame
     pygame.init()
-    screen = pygame.display.set_mode((screen_x, screen_y), HWSURFACE | DOUBLEBUF)
+    screen = pygame.display.set_mode((SCREEN_X, SCREEN_Y), HWSURFACE | DOUBLEBUF)
     pygame.display.set_caption('Fire Propogation')
     clock = pygame.time.Clock()
-    minute_counter = FPS * 60
+    minute_counter = 0
     
     while (1):        
         # Get user input and data from roborealm
@@ -60,27 +29,30 @@ def main():
             if event.type == QUIT:
                 pygame.quit()
                 return
+
+        # Clear the screen            
+        screen.fill((0, 0, 0))  
                 
-        # Deal with things that happen on minute intervals
-        if (minute_counter <= 0):
-            minute_counter = FPS * 60
-            for cell in active_cells:
-                cell.i = min(255, cell.i + 1)
+        # 20 second interval
+        if (minute_counter == FPS * 20):
+            pass            
             
-        screen.fill((0, 0, 0))        
-        
+        # Minute interval
+        if (minute_counter >= FPS * 60):
+            minute_counter = 0      
+            fm.increment_intensity()
+            
         # Propogate the fire
-        
-        
+
         # Draw the fire
-        for cell in active_cells:
-            screen.fill((255, 255 - cell.i, 0), pygame.Rect((cell.x * cell_width, cell.y * cell_height), (cell_width, cell_height)))
+        for (y_pos, x_pos, intensity) in fm.get_fire_grid():
+            screen.fill((255, 255 - intensity, 0), pygame.Rect((x_pos * fm.cell_width, y_pos * fm.cell_height), (fm.cell_width, fm.cell_height)))
         
         # Render to screen
         pygame.display.flip()
 
         # 60 fps
-        minute_counter -= 1
+        minute_counter += 1
         clock.tick(FPS)
         
 if __name__ == "__main__":
